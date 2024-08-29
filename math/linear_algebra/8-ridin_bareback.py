048c137c584aca74415fb15cbba8696be7ab8c45
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-29 13:44:52 -0500

    Weeds out mismatched matrices

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """ Module defines the mat_mul fucntion """
+matrix_shape = __import__('2-size_me_please').matrix_shape
 
 
 def mat_mul(mat1, mat2):
     """ Multiplies two 2D matrices """
+    if matrix_shape(mat1)[1] != matrix_shape(mat2)[0]:
+        return None
     new_mat = []
     for row in mat1:
         new_row = []
