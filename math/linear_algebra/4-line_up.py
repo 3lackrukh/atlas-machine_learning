ca990ef0b2379c654e67b3a5f8cd18380a6db3aa
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-29 11:18:57 -0500

    creates 4-line_up.py. Adds two arrays of the same length element-wise

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+""" Module defines the line_up method """
+
+
+def add_arrays(arr1, arr2):
+    """ Adds two arrays element-wise """
+    if len(arr1) != len(arr2):
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
