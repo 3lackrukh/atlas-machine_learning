4f02aa4210ede4e340a4307aca3ee863c46c1057
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-29 10:48:47 -0500

    creates 2-size_me_please.py. Returns the size of a matrix as a list of integers

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" """
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if matrix:
+            matrix = matrix[0]
+        else:
+            break
+    return shape
