6b1f0823eb53a9503b4eef8da39c0e76f389871d
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-30 14:54:04 -0500

    pycode styling fixes

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ Module defines the np_transpose method """
+
+
 def np_transpose(matrix):
     """ Returns the transpose of a numpy.ndarray """
     return matrix.transpose()
