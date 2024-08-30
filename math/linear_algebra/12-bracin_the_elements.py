699136c026664e16f0887936506d2db30a20e0d4
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-30 15:03:21 -0500

    creates file for task 12. Assigns variables to mathematic operations and returns variables in requested order.

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module defines elementwise method suite for numpy matrix mathematics"""
+
+
+def np_elementwise(mat1, mat2):
+    """ Elementwise addition, subtraction, multiplication, and division """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
