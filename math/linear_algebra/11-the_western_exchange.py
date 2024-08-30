cf45b5ee7df92d7df4abc7da73a077694f47d4a1
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-30 14:52:26 -0500

    creates file for task 11. transposes matrix with the transpose method

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import numpy as np
+""" Module defines the np_transpose method """
+def np_transpose(matrix):
+    """ Returns the transpose of a numpy.ndarray """
+    return matrix.transpose()
