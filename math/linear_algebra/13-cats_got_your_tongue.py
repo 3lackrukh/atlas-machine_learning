10101f04f7ca58fbd72c322f007716a8d7b0277e
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-30 15:17:05 -0500

    creates file for task 13.  uses np.concatenate

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+""" Module defines the np_cat method"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ Concatenates two matrices along specified axis """
+    return np.concatenate((mat1, mat2), axis=axis)
---
+++ math/linear_algebra/13-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import numpy as np
+np_cat = __import__('13-cats_got_your_tongue').np_cat
+
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+mat3 = np.array([[7], [8]])
+print(np_cat(mat1, mat2))
+print(np_cat(mat1, mat2, axis=1))
+print(np_cat(mat1, mat3, axis=1))
+
+""" Expected Output
+[[11 22 33]
+[44 55 66]
+[ 1  2  3]
+ [ 4  5  6]]
+[[11 22 33  1  2  3]
+ [44 55 66  4  5  6]]
+[[11 22 33  7]
+ [44 55 66  8]] """
