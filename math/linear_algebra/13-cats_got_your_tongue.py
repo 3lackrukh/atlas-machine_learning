df518803a976b38505a67829913f00f42684a7eb
3lackrukh <nathan.rhys@atlasschool.com>
2024-08-30 15:21:41 -0500

    increased documentation length line 7

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """ Concatenates two matrices along specified axis """
+    """ numpy concatenate Concatenates two matrices along specified axis """
     return np.concatenate((mat1, mat2), axis=axis)
