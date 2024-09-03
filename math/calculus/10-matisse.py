33519bdef6de15ced9b8c56b51afbd441b132b01
3lackrukh <nathan.rhys@atlasschool.com>
2024-09-03 18:05:11 -0500

    Creates file for task 10. Calculates the derivative of a polynomial

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module defines the poly_derivative method """
+
+
+def poly_derivative(poly):
+    """ Calculates the derivative of a polynomial """
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 0:
+        return None
+    derivative = []
+    for i, coeff in enumerate(poly):
+        if i == 0:
+            continue
+        derivative.append(i * coeff)
+    return derivative
