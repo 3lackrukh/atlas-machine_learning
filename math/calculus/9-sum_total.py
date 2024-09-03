c3a205f8b33e947e0785f31d947eacb1b3384832
3lackrukh <nathan.rhys@atlasschool.com>
2024-09-03 17:47:01 -0500

    sum of squares

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """ Module defines the summation_i_squared method """
 
+
 def summation_i_squared(n):
     """ Calculates the sum of squares of numbers from 1 to n """
-    total = 0
-    for i in range(1, n + 1):
-        total += i ** 2
-    return total
+    try:
+        # Typecast n
+        n = int(n)
+        if n < 0:
+            return None
+        
+        # return the sum of squares
+        return(n* (n + 1) * (2 * n + 1) / 6)
+
+    # Handle n is not an int or float
+    except (ValueError, TypeError):
+        return None
