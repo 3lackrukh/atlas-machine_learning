fb472c35a7dee9aaeac0882ce971592f7840bfb0
3lackrukh <nathan.rhys@atlasschool.com>
2024-09-07 20:31:39 -0500

    expands Poisson class to inculde cdf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -28,3 +28,14 @@ class Poisson:
         for i in range(1, k + 1):
             factorial *= i
         return (e**(-self.lambtha) * (self.lambtha**k)) / factorial
+
+    def cdf(self, k):
+        """ Calculates the CDF for a given number of 'successes' """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
