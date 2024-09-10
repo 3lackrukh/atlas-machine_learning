e409d90d24c798746b0271b77224cdc70b557e90
3lackrukh <nathan.rhys@atlasschool.com>
2024-09-10 14:18:32 -0500

    pycode styling

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -21,6 +21,6 @@ class Binomial:
                 raise ValueError("data must contain multiple values")
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / (len(data))
-            self.p = 1- (variance / mean)
+            self.p = 1 - (variance / mean)
             self.n = round(mean / self.p)
             self.p = mean / self.n
