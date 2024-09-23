d4f929bc19ec68bc0659707869615622d7a05f60
3lackrukh <nathan.rhys@atlasschool.com>
2024-09-22 21:20:24 -0500

    Adds 25-one_hot_decode.py

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 """ Module defines the one_hot_encode method """
 import numpy as np
+
+
+def one_hot_decode(encoded_array):
+    """
+    Decodes a one-hot encoded numpy.ndarray
+    Parameters:
+        encoded_array: numpy.ndarray - shape (classes, m)
+        containing the one-hot encoded labels
+    Returns:
+        decoded_array: numpy.ndarray - shape (m,) containing
+        the numeric labels
+    """
+    if not isinstance(encoded_array, np.ndarray):
+        return None
+    if len(encoded_array.shape) != 2:
+        return None
+    decoded_array = np.argmax(encoded_array, axis=0)
+    return decoded_array
