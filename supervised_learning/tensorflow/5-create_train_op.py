ce6c55e06e411065ca53b88eb7b7f963dfe7b013
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-02 14:53:05 -0500

    Creates 5-create_train_op and main file for task 5

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module defines the create_train_op method """
+import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Creates the training operation for the given loss and alpha.
+
+    Args:
+        loss (tf.Tensor): The loss tensor.
+        alpha (float): The learning rate.
+
+    Returns:
+        tf.Operation: The training operation.
+    """
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+    train_op = optimizer.minimize(loss)
+    return train_op
