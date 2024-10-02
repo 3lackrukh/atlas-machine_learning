4b495e8b85d38b8a8656832d8b28748e17cfe98c
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-01 19:29:18 -0500

    Creates 2-forward_prop and main files for task 2

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module defines the forward_prop method """
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """"
+    Creates the forward propagation graph for the neural network.
+    
+    Parameters:
+        x: placeholder for the input data
+        layer_sizes: list containing the number of nodes in each layer
+        activations: list containing the activation functions for each layer
+    
+    Returns:
+        The prediction of the network in tensor form
+    """
+    create_layer = __import__('1-create_layer').create_layer
+    for i, (size, activation) in enumerate(zip(layer_sizes, activations)):
+        x = create_layer(x, size, activation)
+    return x
