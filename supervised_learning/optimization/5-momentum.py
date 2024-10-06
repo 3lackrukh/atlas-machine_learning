58e2a31d26cc9775b8a32933eac7801a4ba5bfd3
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-06 13:59:44 -0500

    creates 5-momentum and main file for task 5

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+""" Module defines the update_variables_momentum method """
+import numpy as np
+import tensorflow as tf
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using the gradient descent with
+    momentum optimization algorithm.
+
+    Parameters:
+        alpha: floating point learning rate.
+        beta1: floating point momentum weight.
+        var: numpy.ndarray containing variable to be updated.
+        grad: numpy.ndarray gradient of the variable.
+        v: The previous first moment of the variable.
+
+    Returns:
+        var_update: The updated variable.
+        new_moment: The new moment of the variable.
+   """
+    # Calculate the new moment
+    new_moment = beta1 * v + (1 - beta1) * grad
+
+    # Update the variable
+    var_update = var - alpha * new_moment
+
+    return var_update, new_moment
