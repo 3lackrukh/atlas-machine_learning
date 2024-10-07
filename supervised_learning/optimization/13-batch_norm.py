f2821577724b536036bc747bf260831789b268f8
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-07 14:33:21 -0500

    creates 13-batch_norm and main file for task 13

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+import numpy as np
+import numpy as np
+""" Module defines the batch_norm method """
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    Normalizes an unactivated output of a neural network
+    using batch normalization.
+
+    Parameters:
+        Z: numpy.ndarray of shape (m, n)
+            m: integer of data points
+            n: integer of features
+        gamma: numpy.ndarray of shape (1, n)
+            containing the scales for batch normalization
+        beta: numpy.ndarray of shape (1, n)
+            containing the offsets for batch normalization
+        epsilon: small floating point to avoid division by 0
+
+    Returns:
+        Z_norm: numpy.ndarray of shape (m, n)
+            normalized output of neural network
+    """
+    mean = np.mean(Z, axis=0)
+    variance = np.var(Z, axis=0)
+    Z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+    Z_norm = gamma * Z_norm + beta
+    return Z_norm
