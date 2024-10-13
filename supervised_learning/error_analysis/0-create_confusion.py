c25f438102397cb1fe6de72e48d8c622f23baadc
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-13 16:42:41 -0500

    Creates error_analysis folder with README, 0-create_confusion, and main file for task 0

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module defines the create_confusion_matrix method """
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Creates a confusion matrix
+    
+    Parameters:
+        labels: one-hot numpy.ndarray of shape (m, classes)
+            containing correct labels for each data point
+            m: integer count of data points
+            classes: integer count of classes
+        logits: one-hot numpy.ndarray of shape (classes, classes)
+            containing the prediceted labels
+
+    Returns:
+        numpy.ndarray of shape (classes, classes)
+            where row indices represent the correct labels and
+            column indices represent the predicted labels.
+    """
+    return np.matmul(labels.T, logits)
---
+++ supervised_learning/error_analysis/README.md
@@ -0,0 +1,2 @@
+This folder contains files written to practice error analysis
+concepts for Machine Learning in Python.
