fe5328eb4a8d25a587917cd59b14e1a85cd24ee2
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-15 10:15:43 -0500

    creates 4-f1_score.py and main file for task 4

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module defines the f1_score method """
+import numpy as np
+
+
+def f1_score(confusion):
+    """
+    Calculates the F1 score of a confusion matrix
+    
+    Parameters:
+        Confusion: numpy.ndarray of shape (classes, classes)
+            where row indices represent the correct labels
+            and column indices represent the predicted labels
+            classes: the number of classes
+    
+    Returns: numpy.ndarray of shape (classes,)
+            containing the sensitivity of each class
+    """
+    sensitivity = __import__('1-sensitivity').sensitivity(confusion)
+    precision = __import__('2-precision').precision(confusion)
+
+    # Calculate and return f1_score
+    return 2 * (sensitivity * precision) / (sensitivity + precision)
