982db26e5a1263de6092cce965ceb570ff4be352
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-15 16:54:24 -0500

    pycodestyle fixes

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -34,7 +34,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         dW = np.matmul(dZ, A_prev.T) / m
         # Add regularization term
         dW += (lambtha / m) * weights[f'W{layer}']
-    
+
         db = np.sum(dZ, axis=1, keepdims=True) / m
         dA_prev = np.matmul(weights[f'W{layer}'].T, dZ)
 
