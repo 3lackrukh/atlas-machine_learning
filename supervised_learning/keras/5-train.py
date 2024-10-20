2db288490469da9c8a101b8b83b4e483d87cb56d
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-20 12:40:47 -0500

    creates 5-train and main file for task 5

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module defines the train_model method """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a given neural network model using mini-batch gradient descent
+
+    Parameters:
+        network: the model to train
+        data: numpy.ndarray of shape (m, nx) containing the input data
+            m: number of data points
+            nx: number of input features
+        labels: one-hot numpy.ndarray labels for the input data
+        batch_size: integer batch size for gradient descent
+        epochs: integer of passes through data for gradient descent
+        validation_data: data to validate the model with, if not None
+        verbose: boolean determines whether to print training progress
+        shuffle: boolean determines whether to shuffle the data
+
+    Returns:
+        the History object generated after training the model
+    """
+    return network.fit(
+        data, labels, batch_size=batch_size, epochs=epochs,
+        validation_data=validation_data, verbose=verbose, shuffle=shuffle
+    )
