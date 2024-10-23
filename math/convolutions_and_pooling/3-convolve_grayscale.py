af06b9c352bd648a0a9498852c992e27381b7d72
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-23 11:34:43 -0500

    Adds height and width padding for odd height and width values lines 39 - 40

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -36,8 +36,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     if isinstance(padding, tuple):
         ph, pw = padding
     elif padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = ((h - 1) * sh + kh - h) // 2 + (kh % 2 == 0)
+        pw = ((w - 1) * sw + kw - w) // 2 + (kw % 2 == 0)
     elif padding == 'valid':
         ph, pw = 0, 0
 
