23af4086e38169a0d2353ae53135399de38f6cca
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-23 12:51:42 -0500

    removes unnecessary padding logic

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def pool(images, kernel, padding='same', stride=(1, 1), mode='max'):
+def pool(images, kernel, stride=(1, 1), mode='max'):
     """
     Performs a same convolution on grayscale images.
 
@@ -17,11 +17,6 @@ def pool(images, kernel, padding='same', stride=(1, 1), mode='max'):
         convolution.
             kh: integer height of the kernel
             kw: integer width of the kernel
-        padding: either a tuple of (ph, pw), or string 'same' or 'valid'.
-            ph: integer padding height
-            pw: integer padding width
-            'same': output has same height and width as input
-            'valid': no padding
         stride: tuple of (sh, sw)
             sh: integer stride height
             sw: integer stride width
@@ -36,37 +31,22 @@ def pool(images, kernel, padding='same', stride=(1, 1), mode='max'):
     kh, kw = kernel
     sh, sw = stride
     
-    # Calculate padding dimensions
-    if isinstance(padding, tuple):
-        ph, pw = padding
-    elif padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2 + (kh % 2 == 0)
-        pw = ((w - 1) * sw + kw - w) // 2 + (kw % 2 == 0)
-    elif padding == 'valid':
-        ph, pw = 0, 0
-
     # Update dimensions for output array
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
+    output_h = (h - kh) // sh + 1
+    output_w = (w - kw) // sw + 1
     
     # Initialize the output array
-    output = np.zeros((m, output_h, output_w))
-
-    # pad images
-    padded = np.pad(images,
-                    ((0, 0), (ph, ph),
-                     (pw, pw), (0, 0)),
-                    mode='constant')
+    output = np.zeros((m, output_h, output_w, c))
 
     # Perform convolution
     for i in range(output_h):
         for j in range(output_w):
             # get current window
-            window = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            window = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
             # apply kernel
             if mode == 'max':
-                output[:, i, j] = np.max(window, axis=(1, 2, c))
+                output[:, i, j, :] = np.max(window, axis=(1, 2))
             elif mode == 'avg':
-                output[:, i, j] = np.mean(window, axis=(1, 2, c))
+                output[:, i, j, :] = np.mean(window, axis=(1, 2))
 
     return output
