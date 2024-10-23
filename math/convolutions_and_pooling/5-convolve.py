a053882b3c8c21be1b41db426ee1ea4490d32c08
3lackrukh <nathan.rhys@atlasschool.com>
2024-10-23 13:17:00 -0500

    changed variable name line 35

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -18,7 +18,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
             kh: integer height of the kernel
             kw: integer width of the kernel
             kc: integer number of channels in the kernel
-            nk: integer number of kernels
+            nc: integer number of kernels
         padding: either a tuple of (ph, pw), or string 'same' or 'valid'.
             ph: integer padding height
             pw: integer padding width
@@ -32,7 +32,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
         A numpy.ndarray containing the convolved images.
     """
     m, h, w, c = images.shape
-    kh, kw, kc, nk = kernels.shape
+    kh, kw, kc, nc = kernels.shape
     sh, sw = stride
 
     # confirm kernel channel depth
@@ -43,8 +43,8 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     if isinstance(padding, tuple):
         ph, pw = padding
     elif padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2 + (kh % 2 == 0)
-        pw = ((w - 1) * sw + kw - w) // 2 + (kw % 2 == 0)
+        ph = ((h - 1) * sh + kh - h + 1) // 2 + (kh % 2 == 0)
+        pw = ((w - 1) * sw + kw - w + 1) // 2 + (kw % 2 == 0)
     elif padding == 'valid':
         ph, pw = 0, 0
 
@@ -53,7 +53,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     output_w = (w + 2 * pw - kw) // sw + 1
 
     # Initialize the output array
-    output = np.zeros((m, output_h, output_w, nk))
+    output = np.zeros((m, output_h, output_w, nc))
 
     # pad images
     padded = np.pad(images,
@@ -64,7 +64,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     # Perform convolution
     for i in range(output_h):
         for j in range(output_w):
-            for k in range(nk):
+            for k in range(nc):
                 # get current window
                 window = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
                 # apply kernel
