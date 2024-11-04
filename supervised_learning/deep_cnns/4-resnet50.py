ca52376eb6663b442d8ce199f04d577eab1e9594
3lackrukh <nathan.rhys@atlasschool.com>
2024-11-04 16:54:12 -0600

    implemented more standard variable naming conventions for residual stage blocks

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -18,45 +18,45 @@ def resnet50():
     initializer = K.initializers.he_normal(0)
     architecture = [3, 4, 6, 3]
 
-    # First convolutional layer
-    x1 = K.layers.Conv2D(filters=64,
-                         kernel_size=(7, 7),
-                         strides=(2, 2),
-                         padding='same',
-                         kernel_initializer=initializer)(inputs)
-    x1 = K.layers.BatchNormalization(axis=3)(x1)
-    x1 = K.layers.Activation('relu')(x1)
-    x1 = K.layers.MaxPool2D(pool_size=(3, 3),
+    # First stage
+    conv1 = K.layers.Conv2D(filters=64,
+                            kernel_size=(7, 7),
                             strides=(2, 2),
-                            padding='same')(x1)
+                            padding='same',
+                            kernel_initializer=initializer)(inputs)
+    bn1 = K.layers.BatchNormalization(axis=3)(conv1)
+    relu1 = K.layers.Activation('relu')(bn1)
+    pool1 = K.layers.MaxPool2D(pool_size=(3, 3),
+                               strides=(2, 2),
+                               padding='same')(relu1)
 
-    # second convolutional layer
-    x2 = projection_block(x1, [64, 64, 256], s=1)
+    # second stage
+    stage2 = projection_block(pool1, [64, 64, 256], s=1)
     for _ in range(1, architecture[0]):
-        x2 = identity_block(x2, [64, 64, 256])
+        stage2 = identity_block(stage2, [64, 64, 256])
 
-    # third convolutional layer
-    x3 = projection_block(x2, [128, 128, 512], s=2)
+    # third stage
+    stage3 = projection_block(stage2, [128, 128, 512], s=2)
     for _ in range(1, architecture[1]):
-        x3 = identity_block(x3, [128, 128, 512])
+        stage3 = identity_block(stage3, [128, 128, 512])
 
-    # fourth convolutional layer
-    x4 = projection_block(x3, [256, 256, 1024], s=2)
+    # fourth stage
+    stage4 = projection_block(stage3, [256, 256, 1024], s=2)
     for _ in range(1, architecture[2]):
-        x4 = identity_block(x4, [256, 256, 1024])
+        stage4 = identity_block(stage4, [256, 256, 1024])
 
-    # fifth convolutional layer
-    x5 = projection_block(x4, [512, 512, 2048], s=2)
+    # fifth stage
+    stage5 = projection_block(stage4, [512, 512, 2048], s=2)
     for _ in range(1, architecture[3]):
-        x5 = identity_block(x5, [512, 512, 2048])
+        stage5 = identity_block(stage5, [512, 512, 2048])
 
     # Average pooling
-    x6 = K.layers.AveragePooling2D(pool_size=(7, 7),
-                                   padding='same')(x5)
+    pool6 = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                      padding='same')(stage5)
 
     # Fully connected layer
-    x7 = K.layers.Dense(units=1000,
-                        activation='softmax',
-                        kernel_initializer=initializer)(x6)
+    softmax7 = K.layers.Dense(units=1000,
+                              activation='softmax',
+                              kernel_initializer=initializer)(pool6)
 
-    return K.Model(inputs=inputs, outputs=x7)
+    return K.Model(inputs=inputs, outputs=softmax7)
