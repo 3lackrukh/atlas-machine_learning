54d7d9e13410d025a32648a9a55de9665564622b
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-15 17:06:03 -0600

    adds pdf method to class MultiNormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -30,3 +30,35 @@ class MultiNormal():
         # Covariance matrix using n-1 for unbiased estimation
         cov = np.dot(dev, dev.T) / (data.shape[1] - 1)
         self.cov = cov
+        
+    def pdf(self, x):
+        """ Calculates the PDF at a data point
+        Parameters:
+            x: numpy.ndarray of shape (d,) containing the data point
+                d: number of dimensions of the Multivariate Normal distribution
+        Returns:
+            the PDF values for the data point
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        if len(x.shape) != 2\
+            or x.shape[0] != self.cov.shape[1]\
+            or x.shape[1] != 1:
+            raise ValueError(f"x must have the shape ({self.cov.shape[0]}, 1)")
+
+        d = self.cov.shape[1]
+        
+        # Calculate determinant and inverse of covariance matrix
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        
+        # Calculate deviations from mean
+        dev = x - self.mean
+        
+        # Calculate exponential term
+        exp = np.dot(np.dot(dev.T, inv), dev)
+        num = np.exp(-0.5 * exp)
+        
+        # Probability Density Function
+        pdf = float(num / np.sqrt((2 * np.pi) ** d * det))
+        return pdf
