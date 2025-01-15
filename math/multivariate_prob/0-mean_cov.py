297d6c003e1a156f5598a201b2c7b21a9e5a0b82
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-15 15:59:26 -0600

    populates main files, adds mean_cov method to 0-mean_cov

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+""" This module defines the mean_cov method """
+import numpy as np
+
+
+def mean_cov(X):
+    """ Calculates the mean and covariance of a data set
+
+    parameters:
+        X: numpy.ndarray of shape (n, d) containing the data set
+            n: number of data points
+            d: number of dimensions in each data point
+
+    Returns:
+        mean: numpy.ndarray of shape (1, d) containing the mean of the data set
+        cov: numpy.ndarray of shape (d, d) containing the covariance matrix
+            of the data set
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+
+    n, d = X.shape
+    mean = np.mean(X, axis=0, keepdims=True)
+    dev = X - mean
+    cov = np.dot(dev.T, dev) / (n - 1)
+
+    return mean, cov
