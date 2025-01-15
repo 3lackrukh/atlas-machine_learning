19d732d02a2deb6ae36ba1fff8a74dce90725876
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-14 22:34:13 -0600

    creates main file and module for task 5 NO LOOPS/CONDITIONALS

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env
+import numpy as np
+""" Module defines the definiteness method """
+
+
+def definiteness(matrix):
+    """
+    Calculates the definiteness of a matrix
+
+    Parameters:
+        matrix (numpy.ndarray): the matrix to calculate the definiteness of
+
+    Returns:
+        str: the definiteness of the matrix
+
+    Raises:
+        TypeError: if matrix is not a numpy.ndarray
+    """
+    try:
+        validity = (
+            matrix.ndim == 2 and
+            matrix.shape[0] == matrix.shape[1] and
+            np.allclose(matrix, matrix.T)
+        )
+
+        eigenvalues = np.linalg.eigvals(matrix) * validity
+
+        definiteness_map = {
+            (True, False, False, False): "Positive definite",
+            (True, True, False, False): "Positive semi-definite",
+            (False, False, True, False): "Negative definite",
+            (False, False, True, True): "Negative semi-definite"
+        }
+
+        conditions = (
+            np.all(eigenvalues > 0),
+            np.all(eigenvalues >= 0),
+            np.all(eigenvalues < 0),
+            np.all(eigenvalues <= 0)
+        )
+
+        return definiteness_map.get(conditions, "Indefinite"
+                                    if validity else None)
+
+    except AttributeError:
+        raise TypeError("matrix must be a numpy.ndarray")
+    except Exception:
+        return None
