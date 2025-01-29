49131e9cb81737791ae58594e2d1e507b73d091f
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-28 18:21:51 -0600

    Populates main file and adds initialize method for task 0

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
-"""Module defines the 0-initialize method"""
+"""Module defines the initialize method"""
 import numpy as np
+
+
+def initialize(X, k):
+    """
+    Initializes cluster centroids for K-means.
+
+    Parameters:
+        X: numpy.ndarray of size (n, d) containing the dataset.
+            n: int of data points
+            d: int of dimensions for each data point
+        k: int of clusters.
+
+    Returns:
+        numpy.ndarray: The initialized centroids for each cluster.
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    n, d = X.shape
+    centroids = np.zeros((k, d))
+    for i in range(k):
+        centroids[i] = X[np.random.randint(0, n)]
+    return centroids
