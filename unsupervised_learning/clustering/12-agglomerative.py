46dcdb75bdb7569b5ade898edc2ab0fb8a3c7071
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-30 00:04:54 -0600

    populates main file and adds agglomerative method for task 12

--- unsupervised_learning/clustering/12-agglomerative.py
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
-"""Module defines the 12-agglomerative method"""
-import numpy as np
+"""Module defines the agglomerative method"""
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """
+    Performs agglomerative clustering on a dataset with scipy
+
+    Parameters:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        dist: int distance threshold for clustering
+    """
+    hierarchy = scipy.cluster.hierarchy
+    linkage = hierarchy.linkage(X, method='ward')
+    clss = hierarchy.fcluster(linkage, dist, criterion='distance')
+    hierarchy.dendrogram(linkage, color_threshold=dist)
+    plt.show()
+    return clss
