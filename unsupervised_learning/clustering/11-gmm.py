4b768d386b5b9c75e2d7dcf09aa2dc1bded5e1e3
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-29 23:59:49 -0600

    populates main file and adds gmm method for task 11

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
-"""Module defines the 11-gmm method"""
-import numpy as np
+"""Module defines the gmm method"""
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """
+    Performs a gmm on a dataset with sklearn
+
+    Parameters:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        k: int number of clusters
+    """
+    gmm = sklearn.mixture.GaussianMixture(k).fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+    return pi, m, S, clss, bic
