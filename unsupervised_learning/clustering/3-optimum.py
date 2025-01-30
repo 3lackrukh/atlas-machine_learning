b2601e2e0c12aad85e209e48a3732a4b6cdf1d40
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-29 18:06:48 -0600

    populates main file and adds optimum_k method for task 3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python3
-"""Module defines the 3-optimum method"""
+"""Module defines the optimum method"""
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Finds the optimum number of clusters by variance
+
+    Parameters:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        kmin: int the minimum cluster count to evaluate
+                (inclusive)
+        kmax: int the maximum cluster count to evaluate
+                (inclusive)
+        iterations: int the maximum number of iterations
+                allowed in K-means
+    Returns:
+        results: list of K-means outputs at each evaluated cluster count
+        d_vars: list of variance differences between each cluster count and the
+                initial count
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if kmin < 1 or kmin >= X.shape[0]:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if kmax < 1 or kmax <= kmin or kmax >= X.shape[0]:
+        return None, None
+
+    results = []
+    d_vars = []
+
+    for k in range(kmin, kmax + 1):
+        # Run K-means and store results
+        C, clss = kmeans(X, k, iterations=iterations)
+        results.append((C, clss))
+
+        # Calculate and store variance
+        var = variance(X, C)
+        d_vars.append(variance(X, results[0][0]) - var)
+
+    return results, d_vars
