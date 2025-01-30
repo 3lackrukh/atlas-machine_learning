3f64a60384308306480409c0dfc6d37acf5b67d1
3lackrukh <nathan.rhys@atlasschool.com>
2025-01-30 09:44:23 -0600

    Validation ordering lines 20-24

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -18,10 +18,10 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
     if type(kmin) is not int or kmin < 1:
         return None, None, None, None
-    if kmax is None or kmax >= X.shape[0]:
-        kmax = X.shape[0]
     if type(kmax) is not int or kmax <= kmin:
         return None, None, None, None
+    if kmax is None or kmax >= X.shape[0]:
+        kmax = X.shape[0]
     if type(iterations) is not int or iterations < 1:
         return None, None, None, None
     if type(tol) is not float or tol < 0:
