ce98e7a60f548bd6843ba70285bc947a33d1f93f
3lackrukh <nathan.rhys@atlasschool.com>
2025-02-15 20:46:52 -0600

    Adds imports to task files and Project log notebooks

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 """Module defines the 4-bayes_opt method"""
 import numpy as np
+gp = __import__('5-gp').GaussianProcess
+from scipy.stats import norm
--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 """Module defines the 5-bayes_opt method"""
 import numpy as np
+GP = __import__('2-gp').GaussianProcess
