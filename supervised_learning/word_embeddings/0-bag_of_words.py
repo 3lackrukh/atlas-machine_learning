c6dce15106d3dd8d15e8f67b3043c380f7bc0f04
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-06 06:24:41 -0500

    Implements bag_of_words method with helper method: strip

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,2 +1,53 @@
 #!/usr/bin/env python3
-"""Module defines the 0-bag_of_words method"""
+"""Module defines the bag_of_words method"""
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix out of a list of sentences
+
+    Parameters:
+        sentences: List of sentences to analyze
+        vocab: List of vocabulary words to use for analysis
+            if None, all words in sentences will be used
+
+    Returns:
+        embeddings: numpy ndarray of shape(s, f) containing the embeddings
+            s: number of sentences
+            f: number of features analyzed
+        features: List of features used for embeddings
+    """
+    if vocab is None:
+        vocab = []
+        for sentence in sentences:
+            sentence = strip(sentence)
+            for word in sentence.split():
+                if word not in vocab:
+                    vocab.append(word)
+    vocab.sort()
+
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+
+    for i, sentence in enumerate(sentences):
+        sentence = strip(sentence)
+        for word in sentence.split():
+            embeddings[i, vocab.index(word)] += 1
+
+    return embeddings, vocab
+
+
+def strip(sentence):
+    """
+    Strips possession and punctuation from a string
+
+    Parameters:
+        sentence: String to strip
+
+    Returns:
+        Stripped string
+    """
+    import string
+    translator = str.maketrans('', '', string.punctuation)
+    sentence = sentence.replace("'s", "")
+    return sentence.lower().translate(translator)
