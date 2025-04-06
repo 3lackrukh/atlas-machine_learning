d02789803957d7a3da8da0c2288c622cdafcc9eb
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-06 12:15:44 -0500

    Adds fasttext_model function to create and train a gensim FastText model

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env python3
 """Module defines the 4-fasttext method"""
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """
+    Creates, builds, and trains a gensim fastText model
+
+    Parameters:
+        sentences: Lost of sentences to be trained on
+        vector_size: Dimensionality of the embedding layer
+        min_count: minimum number of occurences of a word for use in training
+        negative: size of negative sampling
+        window: maximum distance between current and predicted word
+        cbow: bool determining training type
+            True: CBOW
+            False: Skip-gram
+        epochs: number of epochs to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+
+    Returns:
+        model: trained gensim word2vec model
+    """
+    model = gensim.models.FastText(sentences, vector_size=vector_size,
+                                   min_count=min_count, window=window,
+                                   negative=negative, seed=seed,
+                                   workers=workers, sg=not cbow)
+
+    model.build_vocab(sentences)
+
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
+
+    return model
