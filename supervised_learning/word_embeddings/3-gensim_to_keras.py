2646f3c4413f483e46ecdf61fa230dd5ba42682e
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-06 11:59:01 -0500

    Adds gensim_to_keras function to convert gensim word2vec model to keras Embedding layer

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
-"""Module defines the 3-gensim_to_keras method"""
+"""Module defines the gensim_to_keras method"""
 import tensorflow as tf
+
+
+def gensim_to_keras(model):
+    """
+    Converts a gensim word2vec model to a keras Embedding layer
+
+    Parameters:
+        model: a trained gensim word2vec model
+
+    Returns:
+        the model's keras Embedding layer
+    """
+    # Create a keras Embedding layer with the same weights
+    embedding_layer = tf.keras.layers.Embedding(
+        input_dim=model.wv.vectors.shape[0],
+        output_dim=model.wv.vectors.shape[1],
+        weights=[model.wv.vectors],
+        trainable=True
+    )
+
+    return embedding_layer
