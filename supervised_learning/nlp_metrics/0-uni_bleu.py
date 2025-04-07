5abcf780b38c86f12a02dc43dffa6651522ebe5a
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-07 15:47:01 -0500

    Refactored to use collections.Counter for brevity, removes diagnostic prints, pycodestyle compliant

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -1,73 +1,52 @@
 #!/usr/bin/env python3
 """Module defines the uni_bleu method"""
 import numpy as np
+from collections import Counter
 
 
 def uni_bleu(references, sentence):
     """
     Calculates the unigram BLEU score for a sentence
-    
+
     Parametrs:
         references: list of reference translations
-            each reference translation is a list of the words in the translation
+            each reference translation is a list of words in the translation
         sentence: list containing the model proposed sentence
 
     Returns:
         the unigram BLEU score
     """
-    # Get total number of candidate words
+    # Get candidate length and reference lengths
     len_sen = len(sentence)
-    
-    # Get effective reference length
     ref_lens = [len(reference) for reference in references]
+
+    # Find effective reference length
+    # The closest reference length to the candidate length
     closest_ref_len = min(ref_lens, key=lambda ref_len: abs(ref_len - len_sen))
-    
+
     # Calculate brevity penalty
     if len_sen >= closest_ref_len:
         brevity_penalty = 1
     else:
         brevity_penalty = np.exp(1 - (closest_ref_len / len_sen))
 
+    # Count words in candidate sentence``
+    sentence_counts = Counter(sentence)
 
-    # Dictionary to store count for each word in sentence
-    sentence_dict = {}
-
-    # Iterate over each word in the sentence
-    for word in sentence:
-        # Add each new word to the dictionary
-        if word not in sentence_dict.keys():
-            sentence_dict[word] = 0
-        # Increment the count of the word
-        sentence_dict[word] += 1
-
-    print(f'sentence_dict: {sentence_dict}')
-    
     # Dictionary to store max count of any word in any reference
-    ref_max_dict = {}
+    ref_max_counts = {}
 
-    # Iterate over each word in each reference
+    # Get max word counts across references
     for reference in references:
-        # Dictionary to store count of each word in reference
-        ref_dict = {}
-        for word in reference:
-            if word not in ref_dict.keys():
-                ref_dict[word] = 0
-            # Update the maximum count of the word
-            ref_dict[word] += 1
-            
-        # Update the maximum count of the word in refs_dict
-        for word, count in ref_dict.items():
-            ref_max_dict[word] = max(ref_max_dict.get(word, 0), count)
-        print(f'ref_dict: {ref_dict}')
-        print(f'ref_max_dict: {ref_max_dict}')
+        ref_counts = Counter(reference)
+        for word, count in ref_counts.items():
+            ref_max_counts[word] = max(ref_max_counts.get(word, 0), count)
 
     # Calculate and store clipped counts
-    clipped_count = 0
-    for word, count in sentence_dict.items():
-        # Clip count to maximum references count (or 0 if not in refs_dict)
-        clipped_count += min(count, ref_max_dict.get(word, 0))
+    clipped_count = sum(min(count, ref_max_counts.get(word, 0))
+                        for word, count in sentence_counts.items())
 
     # Calculate precision
     precision = clipped_count / len_sen
-    
+
     return brevity_penalty * precision
