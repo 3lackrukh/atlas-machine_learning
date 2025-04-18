a0a4994bb12bc8f6e2e04c4dc646d9497a35306c
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-18 17:47:23 -0500

    Implement Dataset class for loading and tokenizing translation dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
 """Module defines the Dataset class"""
+import transformers
+import tensorflow_datasets as tfds
 
 
+class Dataset:
+    """class Dataset to load and preps a dataset for machine translation"""
+
+    def __init__(self):
+        """class consrtuctor"""
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='train', as_supervised=True)
+
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='validation',
+            as_supervised=True)
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """
+        Creates subword tokens for our dataset
+
+        Parameters:
+            data: tf.data.Dataset whose examples are formatted as tuple(pt, en)
+                pt: tf.Tensor containing the Portuguese sentence
+                en: tf.Tensor containing the corresponding English sentence
+        Returns:
+            tokenizer_pt: the tokenizer used for the Portuguese sentences
+            tokenizer_en: the tokenizer used for the English sentences
+        """
+        # Initialize pretrained tokenizers
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased",
+            vocab_size=2**15,
+            language="pt")
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            "bert-base-uncased",
+            vocab_size=2**15,
+            language="en")
+
+        return tokenizer_pt, tokenizer_en
