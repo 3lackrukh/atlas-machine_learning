dffc7bf859bcc765810c02c7e316a6480c54b7ad
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-21 17:01:33 -0500

    pycodestyle fixes
    Passes checker, but locally output dimensions different than expected

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -6,29 +6,29 @@ import tensorflow as tf
 def create_masks(inputs, target):
     """
     Creates all masks for training/validation
-    
+
     Parameters:
         inputs: tf.Tensor of shape (batch_size, seq_len_in)
             containing the input sentence
         target: tf.Tensor of shape (batch_size, seq_len_out)
             containing the target sentence
-    
+
     Returns:
         encoder_mask: tf.Tensor padding mask of shape
-            (batch_size, 1, 1, seq_len_in) 
-    
+            (batch_size, 1, 1, seq_len_in)
+
         combined_mask: tf.Tensor of shape
             (batch_size, 1, seq_len_out, seq_len_out)
             the maximum between lookahaed_mask and target padding mask
-        
+
         decoder_mask: tf.Tensor padding mask of shape
             (batch_size, 1, 1, seq_len_in)
             the cross attention mask for the decoder
-            
+
     """
     # Get sequence output length
     seq_len_out = tf.shape(target)[1]
-    
+
     # Create encoder padding mask
     # expand dimensions for multi-head attention
     encoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
@@ -37,11 +37,11 @@ def create_masks(inputs, target):
     # Create look ahead mask for second attention block
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones(
         (seq_len_out, seq_len_out)), -1, 0)
-    
+
     # Create target padding mask
     target_mask = tf.cast(tf.math.equal(target, 0), tf.float32)
     target_mask = target_mask[:, tf.newaxis, tf.newaxis, :]
-    
+
     # Combine the masks
     combined_mask = tf.maximum(target_mask, look_ahead_mask)
 
