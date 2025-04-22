3b8ab06873cfaa22c75064e17c17e4ddeb308002
3lackrukh <nathan.rhys@atlasschool.com>
2025-04-22 12:04:12 -0500

    Refactor 0-qa.py module to include imports and docstring. Loads model and tokenizer, tokenizes question and reference

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
-"""Module defines the 0-qa method"""
+"""Module defines the question_answer method"""
+import tensorflow as tf
+import tensorflow_hub as hub
+from transformers import BertTokenizer
+import numpy as np
+
+
+def question_answer(question, reference):
+    """
+    Finds a snippet of text containing the answer to a question.
+    
+    Parameters
+        question: (str) the question to answer
+        reference: (str) the text to search for the answer
+    Returns
+        answer: (str) the answer to the question 
+    """
+    # Load the pre-trained BERT model and tokenizer
+    bert_model = hub.load("https://www.kaggle.com/models/seesee/bert/TensorFlow2/uncased-tf2-qa/1")
+    tokenizer = BertTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    
+    # Preprocess the input and reference
+    inputs = tokenizer.encode_plus(question, reference, add_special_tokens=True, return_tensors="tf")
