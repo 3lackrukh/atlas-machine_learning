1f908d0e74f80df68dc327630aef5c2e960acec0
3lackrukh <nathan.rhys@atlasschool.com>
2025-05-08 14:35:23 -0500

    Implement q_init function to initialize the Q-table for the FrozenLake environment

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 """Module defines the 1-q_init method"""
+import numpy as np
+import gymnasium as gym
+
+
+def q_init(env):
+    """
+    Initializes the Q-table
+
+    Parameters:
+        env: The FrozenLakeEnv instance
+
+    Returns: numpy.ndarray, The initialized Q-table
+    """
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+    q_table = np.zeros((state_space_size, action_space_size))
+    return q_table
