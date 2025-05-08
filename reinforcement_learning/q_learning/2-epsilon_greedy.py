23318d82d738075f9e11b5ffc6162f84cf745fe3
3lackrukh <nathan.rhys@atlasschool.com>
2025-05-08 15:03:00 -0500

    Implement epsilon-greedy function for action selection in Q-learning

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
 """Module defines the 2-epsilon_greedy method"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Uses epsilon-greedy to determine the next action.
+    
+    Parameters:
+        Q: numpy.ndarray, The Q-table
+        state: int, The current state
+        epsilon: float, The epsilon value for exploration vs. exploitation
+
+    Returns: int, The next action index
+    """
+    p = np.random.uniform(0, 1)
+    if p > epsilon:
+        action = np.argmax(Q[state])
+    else:
+        action = np.random.randint(Q.shape[1])
+    return action 
