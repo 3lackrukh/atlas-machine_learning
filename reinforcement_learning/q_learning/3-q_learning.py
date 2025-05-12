b60fea191351966701b06b166f7f07dd8a626d7e
3lackrukh <nathan.rhys@atlasschool.com>
2025-05-12 14:08:42 -0500

    Implement Q-learning training method with epsilon-greedy action selection

---
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Module defines the train method"""
+import numpy as np
+import gymnasium as gym
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+
+
+def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
+          epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    """
+    Performs Q-learning
+
+    Parameters:
+        env: The FrozenLakeEnv instance
+        Q: numpy.ndarray, The Q-table
+        episodes: int, The total number of episodes to train over
+        max_steps: int, The maximum number of steps per episode
+        alpha: float, The learning rate
+        gamma: float, The discount rate
+        epsilon: float, The initial epsilon value for exploration vs. exploitation
+        min_epsilon: float, The minimum value that epsilon should decay to
+        epsilon_decay: float, The decay rate for updating epsilon between episodes
+
+    Returns: 
+        Q: The updated Q-table
+        total_rewards: list, containing the rewards per episode
+    """
+    total_rewards = []
+    
+    for episode in range(episodes):
+        state, _ = env.reset()
+        ep_reward = 0
+        done = False
+
+        for step in range(max_steps):
+            action = epsilon_greedy(Q, state, epsilon)
+            next_state, reward, done, _, _ = env.step(action)
+
+            # Adjust the reward for the terminal state
+            if done and reward == 0:
+                reward = -1
+
+            # Accumulate episode reward
+            ep_reward += reward
+
+            # Update Q-table - handle terminal state
+            td_target = reward if done else reward + gamma * np.max(Q[next_state])
+            Q[state, action] += alpha * (td_target - Q[state, action])
+
+            # Update state
+            state = next_state
+            if done:
+                break
+        # Record cumulative reward for each episode
+        total_rewards.append(ep_reward)
+
+        # Decay epsilon
+        epsilon = min_epsilon + (epsilon - min_epsilon) * np.exp(
+            -epsilon_decay * episode)
+
+    return Q, total_rewards
