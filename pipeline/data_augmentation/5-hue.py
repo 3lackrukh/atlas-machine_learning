3727e8efb8d541899c41e57835b8b869985bb87d
3lackrukh <nathan.rhys@atlasschool.com>
2025-06-11 17:21:30 -0500

    Adds modified 5-main.py based on example from curriculum. implements 5-hue.py, working as expected

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """Module defines the 5-hue method"""
+import tensorflow as tf
+
+
+def change_hue(image, delta):
+    """
+    Changes the hue of an image
+
+    Args:
+        image: 3D tf.Tensor input image to adjust the hue
+        delta: float amount to change the hue
+
+    Returns:
+        The hue-adjusted image as a tf.Tensor
+    """
+    return tf.image.adjust_hue(image, delta)
--- pipeline/data_augmentation/5-main.py
+++ pipeline/data_augmentation/5-main.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from dataset_loader import get_test_image
+change_hue = __import__('5-hue').change_hue
+
+image = get_test_image(seed=5)
+plt.imshow(change_hue(image, 0.5))
+plt.show()
