4fd22e305f43dbc698613278c0884192e86176bb
3lackrukh <nathan.rhys@atlasschool.com>
2025-06-11 15:10:24 -0500

    adds dataset_loader helper module to load dataset once and re-use from global variable.
    Adds updated main and implementation files for task 0

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 def flip_image(image):
     """
     Flips an image horizontally.
-    
+
     Args:
         image: A 3D tf.Tensor containing the image to flip
-        
+
     Returns:
         The flipped image as a tf.Tensor
     """
