ab03ac5b96b22488aaed17e087edd16bbda5096c
3lackrukh <nathan.rhys@atlasschool.com>
2025-06-11 16:08:46 -0500

    Adds modified 4-main.py based on example from curriculum. implements 4-brightness.py, working as expected

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """Module defines the 4-brightness method"""
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """
+    Randomly changes the brightness of an image.
+
+    Args:
+        image: 3D tf.Tensor input image to adjust the brightness
+        max_delta: float maximum brightness adjustment factor
+
+    Returns:
+        The brightness-adjusted image as a tf.Tensor
+    """
+    return tf.image.random_brightness(image, max_delta)
--- pipeline/data_augmentation/4-main.py
+++ pipeline/data_augmentation/4-main.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from dataset_loader import get_test_image
+change_brightness = __import__('4-brightness').change_brightness
+
+image = get_test_image(seed=4)
+plt.imshow(change_brightness(image, 0.3))
+plt.show()
