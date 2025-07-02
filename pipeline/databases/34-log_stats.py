1abe9bdc6ee9f642ba7b044693d533f22e4f6424
3lackrukh <nathan.rhys@atlasschool.com>
2025-07-02 17:48:10 -0500

    Task 34: Log stats for Nginx logs in MongoDB (Python)

---
+++ pipeline/databases/34-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 34-log_stats.py: Provides stats about Nginx logs stored in MongoDB """
+
+from pymongo import MongoClient
+
+def log_stats():
+    """Displays stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    total = collection.count_documents({})
+    print(f"{total} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
+
+if __name__ == "__main__":
+    log_stats() 
