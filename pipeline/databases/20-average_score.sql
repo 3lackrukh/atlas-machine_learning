6b01a2c390ddf8c9a8d51fe853276a03646dfa75
3lackrukh <nathan.rhys@atlasschool.com>
2025-07-02 17:29:32 -0500

    Task 20: Fix - now uses SELECT INTO to assign avg_score before update (MySQL)

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -3,8 +3,9 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    DECLARE avg_score DECIMAL(5,2);
+    -- Calculate the average score for the user
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = user_id;
+    -- Update the user's average_score
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END;//
-DELIMITER ; 
