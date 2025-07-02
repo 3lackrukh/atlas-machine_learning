4502444e29b19c6281f902b060535206adcc6fdf
3lackrukh <nathan.rhys@atlasschool.com>
2025-07-02 17:16:07 -0500

    Task 16: List Glam rock bands by longevity (MySQL)

---
+++ pipeline/databases/16-glam_rock.sql
@@ -0,0 +1,6 @@
+-- 16-glam_rock.sql
+-- Lists all bands with Glam rock as their main style, ranked by longevity
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC; 
